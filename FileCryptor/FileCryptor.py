8d309e30647f29144b1f188bc7b799c20e05d67b
tabacaruandrei <35452862+tabacaruandrei@users.noreply.github.com>
2022-12-04 21:37:23 +0200

    FileCryptor_0.2
    - placeholdere pentru blocurile try except
    - functia de hash
    - implementarea unei metode de initiale de a mentine consistenta criptarilor prea mari(cand suma dintre ordinul caracterului de criptat si ordinul caracterului corespunzator din sirul de criptare depaseste 255, se pastreaza doar primii 8 biti ai valorii rezultate; cum ascii + ascii extended are valori de la 0 pana la 255, valoarea maxima a unei astfel de sume ar fi 510, cu o lungime in baza 2 de 9 biti; ideea e sa retinem ultimul bit pentru astfel de cazuri pentru a evita pierderi la decriptare)
    - redenumiri la criptare; placeholder-ele isi vor schimba numele in caz ca vor coincide cu cele ale altor fisiere existente
    - imbunatatiri generale

--- FileCryptor/FileCryptor.py
+++ FileCryptor/FileCryptor.py
@@ -1,42 +1,108 @@
 
 import os
+import hashlib
+
+
+def hash(filePath):
+    try:
+        m = hashlib.sha1()
+        f = open(filePath,"rb")
+        while True:
+            data = f.read(4096)
+            if len(data)==0: 
+                break
+            m.update(data)
+        f.close()
+        return m.hexdigest()
+    except:
+        return ""
 
 
 def crypt(file,pw):
-    data = open(file, 'rt', encoding="utf-8")
-    k = 0
-    newfile = os.path.splitext(file)[0] + '_2' + os.path.splitext(file)[1]
-    crypted = open(newfile, 'a+', encoding="utf-8")
-    while(1):
-        char = data.read(1)
-        if not char:
-            data.close()
-            break
-        crypted.write(chr(ord(char) + ord(pw[k%len(pw)])))
-        newchar = chr(ord(char))
-        k += 1
-    crypted.close()
-    os.rename(newfile, file + '.crypted')
+    try:
+        data = open(file, 'rt', encoding="utf-8")
+        k = 0
+        newfile = os.path.splitext(file)[0] + '_2' + os.path.splitext(file)[1]
+        c = 2
+        while os.path.isfile(newfile):
+            c += 1
+            newfile = os.path.splitext(file)[0] + '_' + str(c) + os.path.splitext(file)[1]
+        try:
+            crypted = open(newfile, 'w+', encoding="utf-8")
+            m = hash(file)
+            crypted.write(m)
+            crypted.write('\n\n')
+            lastbits = []
+            while(1):
+                char = data.read(1)
+                if not char:
+                    data.close()
+                    break
+                if(ord(char) + ord(pw[k%len(pw)])) > 255:
+                    newnumber = int(bin(ord(char)+ord(pw[k%len(pw)]))[2:10],2)
+                    lastbits.append(bin(ord(char)+ord(pw[k%len(pw)]))[10])
+                    newchar = chr(newnumber)
+                else:
+                    newchar = chr(ord(char) + ord(pw[k%len(pw)]))
+                crypted.write(newchar)
+                k += 1
+            crypted.seek(0)
+            lines = crypted.readlines()
+            if(len(lastbits) != 0):
+                for i in lastbits:
+                    crypted.write(i)
+                crypted.write('\n')
+            crypted.close()
+            cryptedfile = file + '.crypted'
+            d = 1
+            while os.path.isfile(cryptedfile):
+                d += 1
+                cryptedfile = os.path.splitext(file)[0] + str(d) + os.path.splitext(file)[1] + '.crypted'
+            os.rename(newfile, cryptedfile)
+        except FileExistsError:
+            print('file exists error')
+        except:
+            print('except general 2')
+    except FileNotFoundError:
+        print('file not found error')
+    except:
+        print('except general 1')
 
 
 def decrypt(file,pw):
     newfile = os.path.splitext(os.path.splitext(file)[0])[0] + '_2' + os.path.splitext(os.path.splitext(file)[0])[1]
-    goodfile = os.path.splitext(file)[0] 
-    os.rename(file, newfile)
-    data = open(newfile, 'rt', encoding="utf-8")
-    k = 0
-    crypted = open(goodfile, 'a+', encoding="utf-8")
-    while(1):
-        char = data.read(1)
-        if not char:
+    goodfile = os.path.splitext(file)[0]
+    try:
+        os.rename(file, newfile)
+    except:
+        pass
+    try:
+        data = open(newfile, 'rt', encoding="utf-8")
+        k = 0
+        t = 0 # last bit
+        try:
+            crypted = open(goodfile, 'a+', encoding="utf-8")
+            oghash = data.readline().strip()
+            lastbits = data.readline().strip()
+            while(1):
+                char = data.read(1)
+                if not char:
+                    break
+                if (ord(char) - ord(pw[k%len(pw)])) < 0:
+                    newnumber = ord(char) * 2 - ord(pw[k%len(pw)]) + int(lastbits[t])
+                    newchar = chr(newnumber)
+                    t += 1
+                else:
+                    newchar = chr(ord(char) - ord(pw[k%len(pw)]))
+                crypted.write(newchar)
+                k += 1
             data.close()
-            break
-        newchar = chr(ord(char) - ord(pw[k%len(pw)]))
-        crypted.write(newchar)
-        k += 1
-    data.close()
-    os.remove(newfile)
-    crypted.close()
+            os.remove(newfile)
+            crypted.close()
+        except:
+            pass
+    except:
+        pass
 
 
 if __name__ == "__main__":
