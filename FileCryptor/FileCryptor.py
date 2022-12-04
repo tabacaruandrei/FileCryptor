ee51f9b7faf02a8c92bda12b4fdde2159eeebb3a
tabacaruandrei <35452862+tabacaruandrei@users.noreply.github.com>
2022-12-04 21:43:29 +0200

    FileCryptor_0.3
    - model de executare a criptarii/decriptarii de la tastatura
    - imbunatatirea metodei de mentinere a consistentei criptarii
    - adaugarea unor viitoare exceptii sub forma de if-uri
    - redenumirea fisierului temporar de decriptare
    - imbunatatiri generale

--- FileCryptor/FileCryptor.py
+++ FileCryptor/FileCryptor.py
@@ -1,6 +1,8 @@
 
+import sys
 import os
 import hashlib
+import traceback
 
 
 def hash(filePath):
@@ -31,8 +33,18 @@ def crypt(file,pw):
             crypted = open(newfile, 'w+', encoding="utf-8")
             m = hash(file)
             crypted.write(m)
-            crypted.write('\n\n')
-            lastbits = []
+            crypted.write('\n')
+            lastbits = ""
+            while(1):
+                char = data.read(1)
+                if not char:
+                    break
+                if(ord(char) + ord(pw[k%len(pw)])) > 255:
+                    lastbits += bin(ord(char)+ord(pw[k%len(pw)]))[10]
+            crypted.write(lastbits)
+            crypted.write('\n')
+            data.seek(0)
+            
             while(1):
                 char = data.read(1)
                 if not char:
@@ -40,27 +52,20 @@ def crypt(file,pw):
                     break
                 if(ord(char) + ord(pw[k%len(pw)])) > 255:
                     newnumber = int(bin(ord(char)+ord(pw[k%len(pw)]))[2:10],2)
-                    lastbits.append(bin(ord(char)+ord(pw[k%len(pw)]))[10])
                     newchar = chr(newnumber)
                 else:
                     newchar = chr(ord(char) + ord(pw[k%len(pw)]))
                 crypted.write(newchar)
                 k += 1
-            crypted.seek(0)
-            lines = crypted.readlines()
-            if(len(lastbits) != 0):
-                for i in lastbits:
-                    crypted.write(i)
-                crypted.write('\n')
+            
             crypted.close()
+            
             cryptedfile = file + '.crypted'
             d = 1
             while os.path.isfile(cryptedfile):
                 d += 1
-                cryptedfile = os.path.splitext(file)[0] + str(d) + os.path.splitext(file)[1] + '.crypted'
+                cryptedfile = os.path.splitext(file)[0] + '_' + str(d) + os.path.splitext(file)[1] + '.crypted'
             os.rename(newfile, cryptedfile)
-        except FileExistsError:
-            print('file exists error')
         except:
             print('except general 2')
     except FileNotFoundError:
@@ -70,20 +75,32 @@ def crypt(file,pw):
 
 
 def decrypt(file,pw):
+    # if not os.path.isfile(file):
+    #     print('fisierul nu exista.')
+    # if os.path.splitext(file)[1] != '.crypted':
+    #     print('fisierul are formatul gresit (nu e .crypted la final)')
+    # elif os.path.splitext(os.path.splitext(file)[0])[1] != '.txt':
+    #     print('fisierul are formatul gresit (nu e .txt.crypted)')
     newfile = os.path.splitext(os.path.splitext(file)[0])[0] + '_2' + os.path.splitext(os.path.splitext(file)[0])[1]
     goodfile = os.path.splitext(file)[0]
-    try:
-        os.rename(file, newfile)
-    except:
-        pass
+    
+    c = 2
+    while os.path.isfile(newfile):
+        c += 1
+        newfile = os.path.splitext(os.path.splitext(file)[0])[0] + '_' + str(c) + os.path.splitext(os.path.splitext(file)[0])[1]
+    os.rename(file, newfile)
+    
     try:
         data = open(newfile, 'rt', encoding="utf-8")
         k = 0
         t = 0 # last bit
         try:
             crypted = open(goodfile, 'a+', encoding="utf-8")
+            
             oghash = data.readline().strip()
+            
             lastbits = data.readline().strip()
+            
             while(1):
                 char = data.read(1)
                 if not char:
@@ -103,6 +120,22 @@ def decrypt(file,pw):
             pass
     except:
         pass
+
+
+def execinput():
+    # INPUT: FileCryptor.py crypt abc.exe my_pass ⇒ va crea fisierul abc.exe.crypted
+    # INPUT: FileCryptor.py decrypt abc.exe.cryped my_pass ⇒ va crea fișierul abc.exe
+    if (len(sys.argv) - 1) < 3:
+        print('Sintaxa incorecta1. Utilizati formatul:\n')
+        print('FileCryptor.py [crypt/decrypt] path_fisier parola_criptare')
+    else:
+        if(sys.argv[1] == 'crypt'):
+            crypt(sys.argv[2], sys.argv[3])
+        elif(sys.argv[1].lower == 'decrypt'):
+            decrypt(sys.argv[2], sys.argv[3])
+        else: 
+            print('Sintaxa incorecta. Utilizati formatul:\n')
+            print('FileCryptor.py [crypt/decrypt] [path_fisier] [parola_criptare]')
 
 
 if __name__ == "__main__":
