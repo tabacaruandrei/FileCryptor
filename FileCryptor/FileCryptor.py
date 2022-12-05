58df35e3727838453df91393074f63981e381be7
tabacaruandrei <35452862+tabacaruandrei@users.noreply.github.com>
2022-12-05 15:16:41 +0200

    FileCryptor_0.7
    - functionalitate independenta de formatul fisierului
    - functie de testare multipla(pentru verificare la laborator)

--- FileCryptor/FileCryptor.py
+++ FileCryptor/FileCryptor.py
@@ -17,7 +17,7 @@ def hash(filePath,fileSize):
         return m.hexdigest()
     except:
         return ""
-
+    
 
 def crypt(file,pw):
     try: 
@@ -25,6 +25,8 @@ def crypt(file,pw):
             raise Exception("Path-ul introdus nu corespunde unui fisier. Introduceti path-ul unui fisier.")
         if os.path.getsize(file) == 0:
             raise Exception("Fisierul introdus este gol. Introduceti date valide.")
+        if os.path.splitext(file)[1] == '.crypted':
+            raise Exception('Fisierul este deja criptat. Introduceti date valide.')
         
         data = open(file, 'rt', encoding="utf-8")
         k = 0
@@ -79,6 +81,7 @@ def crypt(file,pw):
             os.rename(newfile, cryptedfile)
             print('Criptare realizata cu succes.')
             print('Locatie fisier criptat:', cryptedfile)
+            return cryptedfile
         except:
             print('Au aparut probleme in timpul procesului de criptare. Introduceti date valide.')
     except Exception as e:
@@ -92,10 +95,8 @@ def decrypt(file,pw):
         if not os.path.isfile(file):
             raise Exception("Path-ul introdus nu corespunde unui fisier. Introduceti path-ul unui fisier.")
         if os.path.splitext(file)[1] != '.crypted':
-            raise Exception('Fisierul nu are formatul corespunzator unui fisier criptat(".txt.crypted"). Introduceti date valide.')
-        elif os.path.splitext(os.path.splitext(file)[0])[1] != '.txt':
-            raise Exception('Fisierul nu are formatul corespunzator unui fisier criptat(".txt.crypted"). Introduceti date valide.')
-        newfile = os.path.splitext(os.path.splitext(file)[0])[0] + '_1' + os.path.splitext(os.path.splitext(file)[0])[1]
+            raise Exception('Fisierul nu are formatul corespunzator unui fisier criptat(".crypted"). Introduceti date valide.')
+        newfile = os.path.splitext(os.path.splitext(file)[0])[0] + '_1' + '.txt'
         goodfile = os.path.splitext(file)[0]
         
         c = 1
@@ -143,6 +144,7 @@ def decrypt(file,pw):
             if(newhash == oghash):
                 print('Decriptare realizata cu succes.')
                 print('Locatie fisier decriptat:', goodfile)
+                return goodfile
             else:
                 print('Decriptare esuata. Introduceti date valide.')
         except Exception as e:
@@ -159,17 +161,44 @@ def execinput():
     # INPUT: FileCryptor.py crypt abc.exe my_pass ⇒ va crea fisierul abc.exe.crypted
     # INPUT: FileCryptor.py decrypt abc.exe.crypted my_pass ⇒ va crea fișierul abc.exe
     if (len(sys.argv) - 1) < 3:
-        print('Sintaxa incorecta1. Utilizati formatul:\n')
+        print('Sintaxa incorecta. Utilizati formatul:\n')
         print('FileCryptor.py [crypt/decrypt] path_fisier parola_criptare')
     else:
-        if(sys.argv[1].lower() == 'crypt'):
-            crypt(sys.argv[2], sys.argv[3])
-        elif(sys.argv[1].lower() == 'decrypt'):
-            decrypt(sys.argv[2], sys.argv[3])
-        else: 
-            print('Sintaxa incorecta. Utilizati formatul:\n')
-            print('FileCryptor.py [crypt/decrypt] [path_fisier] [parola_criptare]')
+        if (len(sys.argv) - 1) == 3:
+            if(sys.argv[1].lower() == 'crypt'):
+                crypt(sys.argv[2], sys.argv[3])
+            elif(sys.argv[1].lower() == 'decrypt'):
+                decrypt(sys.argv[2], sys.argv[3])
+            else: 
+                print('Sintaxa incorecta. Utilizati formatul:\n')
+                print('FileCryptor.py [crypt/decrypt] [path_fisier] [parola_criptare]')
+        elif(len(sys.argv) - 1) == 4: 
+            if(sys.argv[1].lower() == 'multitest'):
+                multitest(sys.argv[2], sys.argv[3], sys.argv[4])
+            
 
-
+def multitest(file,pw,k):
+    try:
+        if not k.isdigit():
+            raise Exception('Furnizati un numar de criptari efectuate integral.')
+        if(int(k) < 1):
+            raise Exception('Furnizati un numar de criptari efectuate strict pozitiv.')
+        k = int(k)
+        funcs = [crypt,decrypt]
+        finalfile = file[:]
+        n = 0
+        while k != 0:
+            finalfile = funcs[n % 2](finalfile,pw)[:]
+            k -= 1
+            n += 1
+        print('Testare realizata cu succes.')
+        # print('Locatie fisier final:', cryptedfile)
+        
+    except Exception as e:
+        print(e)
+    except:
+        print('A aparut o eroare in timpul procesului de testare. Introduceti date valide.')
+    
+    
 if __name__ == "__main__":
     execinput()
